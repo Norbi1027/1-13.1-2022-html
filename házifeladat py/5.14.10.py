a9b373468710896c64bda66200d9606443c9166d
Norbi1027 <sandornorbi7@gmail.com>
2022-11-29 10:02:52 +0100

    py

---
+++ házifeladat py/5.14.10.py
@@ -0,0 +1,10 @@
+def atfogo():
+    megoldas = 0
+    x = 0
+    egy = int(input("Első befogó"))
+    masodik = int(input("masodik befogó"))
+    x = (egy*egy)+(masodik*masodik)
+    megoldas = x ** 0.5
+
+    print(megoldas)
+atfogo()
---
+++ házifeladat py/6.9.1.py
@@ -0,0 +1,28 @@
+
+class teszt:
+    def irany(irany):
+        if irany == "é" and "É":
+            print("Kelet")
+        elif irany == "k" and "K":
+            print("Dél")
+        elif irany == "d" and "D":
+            print("NYugat")
+        elif irany == "ny" and "NY":
+            print("Észak")
+        else:
+            print("none")
+        return irany
+    def nap_nev(nap):
+        napok = ["hétfő","kedd","szerda","csütörtök","péntek","szombat","vasárnap"]
+        if nap <= 0 and nap >= 0:
+            print(napok[nap])
+        else:
+            print("none")
+    def nap_sorszam(nap):
+        napok = ["hétfő","kedd","szerda","csütörtök","péntek","szombat","vasárnap"]
+        print (napok[len(nap)])
+
+teszt.irany("é")
+teszt.nap_nev(7)
+teszt.nap_sorszam("hétfő")
+
